6a0ed65150cdb77a14f311cc44684abd3e446632
Ben Grbowski <bjgrabow@calpoly.edu>
2018-09-24 22:52:06 -0700

    Added weight calculation functionality

--- planets.py
+++ planets.py
@@ -1,6 +1,7 @@
 def weight_on_planets():
    # write your code here
 
+   #weight is an integer to be input by the user
    weight = int(input('What do you weigh on earth? '))
 
    print('\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds.' % ((weight * 0.38),(weight * 2.34)))
